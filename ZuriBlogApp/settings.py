a16f96b8435b55b817157986fc6ead1649f010af
Oluwatosin Olasupo <oluwateeblaq@gmail.com>
2021-04-27 18:22:28 +0100

    SMTP Debug

--- ZuriBlogApp/settings.py
+++ ZuriBlogApp/settings.py
@@ -26,8 +26,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = False
+DEBUG = True
+# DEBUG = False
 
 ALLOWED_HOSTS = ['zuridjangoblog.herokuapp.com', '127.0.0.1', 'localhost']
 
